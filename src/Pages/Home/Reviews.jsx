d85933fd92653b50aa53647b3a6a79de279aeeee
Mehedi Hassan <rehan51322@gmail.com>
2022-05-28 09:29:53 +0600

    features added and homepage

--- src/Pages/Home/Reviews.jsx
+++ src/Pages/Home/Reviews.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import Loading from '../Shared/Loading';
-import Review from './Review';
+import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
+import Review from "./Review";
 
 const Reviews = () => {
-   const {
-     data: reviews,
-     isLoading,
-     refetch,
-   } = useQuery("reviews", () =>
-     fetch(`http://localhost:5000/review`).then((res) =>
-       res.json()
-     )
-   );
+  const {
+    data: reviews,
+    isLoading,
+    refetch,
+  } = useQuery("reviews", () =>
+    fetch(`http://localhost:5000/review`).then((res) => res.json())
+  );
 
-   if (isLoading) {
-     return <Loading></Loading>;
-   }
-   return (
-     <section className="my-28">
-       <div className="grid justify-center mb-5 mt-0">
-         <div>
-           <h2 className="text-primary font-semibold text-3xl">
-             Our Clients Reviews
-           </h2>
-         </div>
-       </div>
-       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-         {reviews.reverse().map((review) => (
-           <Review key={review._id} review={review}></Review>
-         ))}
-       </div>
-     </section>
-   );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+  return (
+    <section className="my-28">
+      <div className="flex flex-col items-center justify-center mb-3">
+        <h1 className="lg:text-4xl text-3xl font-bold text-center text-primary">
+          Hear from our clients
+        </h1>
+        <p className="text-base leading-6 mt-4 text-center text-secondary 2xl:w-2/5 ">
+          Here is why you should trust us with your work achievements
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {reviews.reverse().map((review) => (
+          <Review key={review._id} review={review}></Review>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Reviews;
